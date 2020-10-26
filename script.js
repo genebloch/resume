d0d34e46a51f5568a7bdcfdb1962335529b8d13b
Übermensch <genebloch00@gmail.com>
2020-10-26 18:21:26 +0300

    Resume

---
+++ script.js
@@ -0,0 +1,8 @@
+new fullpage('#fullpage', {
+    anchors: ['intro', 'about', 'skills', 'hobbies'],
+    sectionsColor: ['#0ead69', '#0582ca', '#f25f5c', '#ffbc42'],
+    slidesNavigation: true,
+    navigation: true,
+    controlArrows: true,
+    scrollingSpeed: 1000
+});
